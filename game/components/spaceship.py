c024e6fe18d5a155a5663f8b275f0f589ae2508d
SantiagoPalomar <124805965+SantiagoPalomar@users.noreply.github.com>
2023-07-14 17:44:30 -0500

    Clase 1 Tarea: Se transporta cuando llega al limite de un lado al otro

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -15,29 +15,37 @@ class Spaceship(Sprite):
         
     def update(self, user_input):
         if user_input[pygame.K_LEFT]:
-            self.move_left
+            self.move_left()
         elif user_input[pygame.K_RIGHT]:
-            self.move_right
+            self.move_right()
         elif user_input[pygame.K_DOWN]:
-            self.move_down
+            self.move_down()
         elif user_input[pygame.K_UP]:
-            self.move_up
+            self.move_up()
 
     def move_left(self):
         if self.rect.left > 0:
             self.rect.x -= 10
+        else:
+            self.rect.x = SCREEN_WIDTH - self.rect.width
 
     def move_right(self):
         if self.rect.right < SCREEN_WIDTH:
             self.rect.x += 10
+        else:
+            self.rect.x = 0
 
     def move_down(self):
         if self.rect.bottom < SCREEN_HEIGHT:
             self.rect.y -= 10
+        else:
+            self.rect.y = 0
 
     def move_up(self):
-        if self.rect.top > SCREEN_HEIGHT:
+        if self.rect.top > 0:
             self.rect.y -= 10
+        else:
+            self.rect.y = SCREEN_HEIGHT - self.rect.height
     
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
